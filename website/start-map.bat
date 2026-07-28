58995e2bac2222eb259010ad271eeae3072af1b5
Mrdben <mrdben09@gmail.com>
2026-07-28 09:43:14 -0400

    Build branded project homepage and bridge map

--- website/start-map.bat
+++ website/start-map.bat
@@ -2,12 +2,12 @@
 setlocal
 cd /d "%~dp0"
 
-echo Starting Ben2 Bridges at http://127.0.0.1:8000/
+echo Starting Ben2 Bridges at http://127.0.0.1:8000/index.html
 echo Keep this window open while using the map.
 echo Press Ctrl+C or close this window to stop the server.
 echo.
 
-start "" powershell.exe -NoProfile -WindowStyle Hidden -Command "Start-Sleep -Seconds 1; Start-Process 'http://127.0.0.1:8000/'"
+start "" powershell.exe -NoProfile -WindowStyle Hidden -Command "Start-Sleep -Seconds 1; Start-Process 'http://127.0.0.1:8000/index.html'"
 python -m http.server 8000 --bind 127.0.0.1
 
 if errorlevel 1 (
